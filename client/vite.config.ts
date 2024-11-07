07bddcadb5fca984c4e34c095ea5476568d11cf8
Geunwoo Kim <9eunwoo.kim@gmail.com>
2024-11-08 00:41:43 +0900

    feat: Demo 페이지 레이아웃 구현

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+const { dirname } = import.meta;
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(dirname, 'src'),
+      '@components': resolve(dirname, 'src/components'),
+    },
+  },
 });
