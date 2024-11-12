9a46c547ea6bdeaf03bce77bba527fb2bc74e6e0
suhyun-00 <unhyns00@gmail.com>
2024-11-12 18:01:34 +0900

    feat: VideoGrid 컴포넌트에 본인 화면 출력

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import StudyRoom from '@components/StudyRoom/StudyRoom';
 
 function App() {
   return (
-    <div className="font-pretendard text-gomz-black flex justify-center">
+    <div className="font-pretendard text-gomz-black bg-gomz-white flex h-screen w-screen justify-center">
       <StudyRoom />
     </div>
   );
