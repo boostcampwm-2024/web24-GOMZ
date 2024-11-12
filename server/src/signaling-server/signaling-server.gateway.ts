a9c9bbb48a636171028edaf66e11e95f2d5a07b9
Choi-JY <Choi-JY1107@github.com>
2024-11-12 22:47:45 +0900

    refactor: room 관련 모듈 분리

--- server/src/signaling-server/signaling-server.gateway.ts
+++ server/src/signaling-server/signaling-server.gateway.ts
@@ -11,31 +11,31 @@ import { Socket, Server } from 'socket.io';
 import { Inject } from '@nestjs/common';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import { StudyRoomsService } from '../study-room/study-room.service';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class SignalingServerGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER)
     private readonly logger: Logger,
-  ) {}
-
-  // 방에 들어있는 사용자의 소켓 정보
-  users = [];
+    private readonly studyRoomsService: StudyRoomsService,
+  ) { }
 
   @WebSocketServer()
   server: Server;
 
-  // 1. 신규 참가자가 접속을 요청한다. 그리고 방에 있는
-  // 기존참가자들 소켓 정보를 반환한다.
+  // 1. 신규 참가자가 접속을 요청한다. 그리고 방에 있는 기존 참가자들 소켓 정보를 반환한다.
   handleConnection(client: Socket) {
     this.logger.info(`${client.id} 접속!!!`);
-    client.emit('offerRequest', JSON.stringify({ users: this.users }));
-    this.users.push(client.id);
+    const defaultRoom = '1';
+    this.studyRoomsService.addUserToRoom(defaultRoom, client.id);
+    const users = this.studyRoomsService.getRoomUsers(defaultRoom).filter((id) => id !== client.id);
+    client.emit('offerRequest', JSON.stringify({ users }));
   }
 
   handleDisconnect(client: Socket) {
     this.logger.info(`${client.id} 접속해제!!!`);
-    this.users = this.users.filter((user) => user !== client.id);
+    this.studyRoomsService.leaveAllRooms(client.id);
   }
 
   // 2. 신규 참가자가 기존 참가자들에게 offer를 보낸다.
@@ -45,7 +45,7 @@ export class SignalingServerGateway implements OnGatewayConnection, OnGatewayDis
     @MessageBody('offer') offer: RTCSessionDescriptionInit,
     @MessageBody('oldId') oldId: string,
   ) {
-    this.logger.info(`new user: ${client.id} sends an offer to old user: ${oldId} `);
+    this.logger.info(`new user: ${client.id} sends an offer to old user: ${oldId}`);
     this.server.to(oldId).emit('answerRequest', JSON.stringify({ newId: client.id, offer }));
   }
 
@@ -56,7 +56,7 @@ export class SignalingServerGateway implements OnGatewayConnection, OnGatewayDis
     @MessageBody('answer') answer: RTCSessionDescriptionInit,
     @MessageBody('newId') newId: string,
   ) {
-    this.logger.info(`old user: ${client.id} sends an answer to new user: ${newId} `);
+    this.logger.info(`old user: ${client.id} sends an answer to new user: ${newId}`);
     this.server.to(newId).emit('completeConnection', JSON.stringify({ oldId: client.id, answer }));
   }
 
@@ -66,9 +66,7 @@ export class SignalingServerGateway implements OnGatewayConnection, OnGatewayDis
     @MessageBody('targetId') targetId: string,
     @MessageBody('iceCandidate') candidate: RTCIceCandidateInit,
   ) {
-    this.logger.info(`user: ${client.id} send ice candidate to user: ${targetId}`);
-    this.server
-      .to(targetId)
-      .emit('setIceCandidate', JSON.stringify({ senderId: client.id, iceCandidate: candidate }));
+    this.logger.info(`user: ${client.id} sends ICE candidate to user: ${targetId}`);
+    this.server.to(targetId).emit('setIceCandidate', JSON.stringify({ senderId: client.id, iceCandidate: candidate }));
   }
 }
