dcabfc930f9d213486993beb93c79c586af50459
Choi-JY1107 <cjy11230@cau.ac.kr>
2024-11-12 23:16:54 +0900

    test: signalling-server 테스트 시, logger 의존성이 없어서 오류가 나던 현상 수정

--- server/src/signaling-server/signaling-server.gateway.spec.ts
+++ server/src/signaling-server/signaling-server.gateway.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SignalingServerGateway } from './signaling-server.gateway';
+import { StudyRoomsService } from '../study-room/study-room.service';
+import { MockStudyRoomRepository } from '../study-room/mock.repository';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 describe('SignalingServerGateway', () => {
   let gateway: SignalingServerGateway;
+  let logger: Logger;
+  let studyRoomsService: StudyRoomsService;
 
   beforeEach(async () => {
+    // Mock Logger 생성
+    const mockLogger = {
+      info: jest.fn(),
+      error: jest.fn(),
+      warn: jest.fn(),
+    };
+
+    const mockRepository = new MockStudyRoomRepository();
+    studyRoomsService = new StudyRoomsService(mockRepository);
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SignalingServerGateway],
+      providers: [
+        SignalingServerGateway,
+        { provide: WINSTON_MODULE_PROVIDER, useValue: mockLogger },
+        { provide: StudyRoomsService, useValue: studyRoomsService },
+      ],
     }).compile();
 
     gateway = module.get<SignalingServerGateway>(SignalingServerGateway);
+    logger = module.get<Logger>(WINSTON_MODULE_PROVIDER);
   });
 
-  it('should be defined', () => {
+  it('SignalingServerGateway가 정의되어야 합니다.', () => {
     expect(gateway).toBeDefined();
+  });
+
+  it('사용자가 접속하면 logger.info가 호출되어야 합니다.', () => {
+    const clientMock = { id: 'user1', emit: jest.fn() } as any;
+    gateway.handleConnection(clientMock);
+    expect(logger.info).toHaveBeenCalledWith('user1 접속!!!');
+  });
+
+  it('사용자가 접속 해제하면 logger.info가 호출되어야 합니다.', () => {
+    const clientMock = { id: 'user1' } as any;
+    gateway.handleDisconnect(clientMock);
+    expect(logger.info).toHaveBeenCalledWith('user1 접속해제!!!');
   });
 });
