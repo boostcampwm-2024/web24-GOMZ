218fee99b3c367a17b5eec65ebaff972282897a4
stupidJoon <junsang.yu3@gmail.com>
2024-11-13 20:33:20 +0900

    test: 시그널링 서버 테스트 코드 작성

--- server/src/signaling-server/signaling-server.module.ts
+++ server/src/signaling-server/signaling-server.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SignalingServerGateway } from './signaling-server.gateway';
-import { StudyRoomModule } from 'src/study-room/study-room.module';
+import { StudyRoomModule } from '../study-room/study-room.module';
 
 @Module({
   imports: [StudyRoomModule],
