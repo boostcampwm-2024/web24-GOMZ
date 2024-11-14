86bd1550c43a84374e627780711be85629b6582a
Geunwoo Kim <9eunwoo.kim@gmail.com>
2024-11-14 13:21:37 +0900

    design: 메인 페이지 배치 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import Home from '@components/Home';
 
 function App() {
   return (
-    <div className="font-pretendard text-gomz-black flex min-h-screen w-screen justify-center">
+    <div className="font-pretendard text-gomz-black">
       <Home />
     </div>
   );
