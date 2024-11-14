97a6752e61e2630114138fe462f3de34ac7b0872
solyrion <sde101573@gmail.com>
2024-11-14 12:12:06 +0900

    feat: 방 생성 + 존재하는 방 리스트 Api 구현

--- server/src/study-room/study-room.module.ts
+++ server/src/study-room/study-room.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudyRoomsService } from './study-room.service';
 import { MockStudyRoomRepository } from './mock.repository';
+import { StudyRoomController } from './study-room.controller';
 
 @Module({
   providers: [StudyRoomsService, MockStudyRoomRepository],
   exports: [StudyRoomsService],
+  controllers: [StudyRoomController],
 })
 export class StudyRoomModule {}
