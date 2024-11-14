a3e7b208492cba096fcb95abe5ced7a71a67ef0c
solyrion <sde101573@gmail.com>
2024-11-14 15:28:41 +0900

    feat: 닉네임 생성 매서드 controller 적용

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,7 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { generateRandomNickname } from '../util/nicknameGenerator';
 
 @Controller('user')
 export class UserController {
-  @Get('/random')
-  getRandom() {}
+  @Get('/random-name')
+  getRandomName(): { nickName: string } {
+    // random 닉네임 호출
+    const nickName = generateRandomNickname();
+    return { nickName };
+  }
 }
