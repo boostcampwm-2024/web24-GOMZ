e0680231ffbe652e2f508bca30f30b30795b66eb
Geunwoo Kim <9eunwoo.kim@gmail.com>
2024-11-14 14:09:30 +0900

    feat: 3주차 데모용 React Router 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
-import Home from '@components/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ProtectedRoute from '@components/common/ProtectedRoute';
+import Home from '@components/Home/Home';
+import StudyRoom from '@components/StudyRoom/StudyRoom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/studyroom',
+    element: (
+      <ProtectedRoute>
+        <StudyRoom />
+      </ProtectedRoute>
+    ),
+  },
+]);
 
 function App() {
   return (
     <div className="font-pretendard text-gomz-black">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
