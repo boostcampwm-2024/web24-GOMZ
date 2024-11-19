d5b460f6e813a0d5477b10edca07391ccd0ee401
Choi-JY1107 <Choi-JY1107@github.com>
2024-11-19 11:27:05 +0900

    fix: 채팅 서버에서 disconnect 시, leaveRoom을 요청하는 현상 수정

--- server/src/chatting-server/chatting-server.gateway.ts
+++ server/src/chatting-server/chatting-server.gateway.ts
@@ -33,36 +33,6 @@ export class ChattingServerGateway implements OnGatewayConnection, OnGatewayDisc
   // 사용자가 접속을 해제했을 때
   handleDisconnect(client: Socket) {
     this.logger.info(`${client.id} 접속 해제!`);
-    this.studyRoomsService.leaveAllRooms(client.id);
-  }
-
-  // 특정 방에 참가 요청을 받음
-  @SubscribeMessage('joinRoom')
-  handleJoinRoom(@ConnectedSocket() client: Socket, @MessageBody('room') roomId: string) {
-    const room = this.studyRoomsService.findRoom(roomId);
-
-    if (!room) {
-      // 방이 존재하지 않으면 생성
-      this.studyRoomsService.createRoom(roomId, client.id);
-    }
-
-    client.join(roomId);
-    this.studyRoomsService.addUserToRoom(roomId, client.id);
-    this.logger.info(`${client.id}님이 방 ${roomId}에 입장했습니다.`);
-
-    // 방에 있는 다른 사용자들에게 알림
-    this.server.to(roomId).emit('userJoined', JSON.stringify({ userId: client.id }));
-  }
-
-  // 특정 방에서 퇴장 요청을 받음
-  @SubscribeMessage('leaveRoom')
-  handleLeaveRoom(@ConnectedSocket() client: Socket, @MessageBody('room') roomId: string) {
-    client.leave(roomId);
-    this.studyRoomsService.removeUserFromRoom(roomId, client.id);
-    this.logger.info(`${client.id}님이 방 ${roomId}에서 나갔습니다.`);
-
-    // 방에 있는 다른 사용자들에게 알림
-    this.server.to(roomId).emit('userLeft', JSON.stringify({ userId: client.id }));
   }
 
   // 메시지 전송
