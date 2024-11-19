ba043ff522644ab5c32ee03329f1969e65ecac92
Choi-JY1107 <cjy11230@kakao.com>
2024-11-19 16:14:27 +0900

    feat: study-room entity를 실제 DB와 적용되도록 구현

--- server/src/study-room/study-room.entity.ts
+++ server/src/study-room/study-room.entity.ts
@@ -1,9 +1,19 @@
-export class StudyRoom {
-  roomId: string;
-  users: string[];
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
 
-  constructor(roomId: string) {
-    this.roomId = roomId;
-    this.users = [];
-  }
+@Entity('study_room')
+export class StudyRoom {
+  @PrimaryGeneratedColumn()
+  room_id: number;
+
+  @Column({ type: 'varchar', length: 45, nullable: false })
+  room_name: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @Column({ type: 'int', nullable: false })
+  category_id: number;
+
+  @Column({ type: 'int', nullable: false })
+  user_id: number;
 }
