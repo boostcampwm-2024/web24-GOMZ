f8b60ab89024506a29bfe7c82e69b5a66ca7aa6a
solyrion <sde101573@gmail.com>
2024-11-19 16:37:40 +0900

    feat: 전역으로 CORS 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
