3c40bf32a9069919b5573a1100557964731e15a8
Choi-JY1107 <cjy11230@kakao.com>
2024-11-19 16:56:12 +0900

    fix: 수정된 DB 스키마에 맞게 entity 코드 수정

---
+++ server/src/study-room/study-room-participant.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+@Entity('study_room_participant')
+export class StudyRoomParticipant {
+  @PrimaryColumn({ type: 'varchar', length: 45 })
+  socket_id: string;
+
+  @Column({ type: 'varchar', length: 45 })
+  nickname: string;
+
+  @Column({ type: 'int', nullable: false })
+  room_id: number;
+}
