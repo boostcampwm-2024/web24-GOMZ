abcf5b29af6b55d2c88ee81e1cd3b68f57f8a32c
suhyun-00 <unhyns00@gmail.com>
2024-11-21 00:40:31 +0900

    StudyRoomList 라우터 연결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ProtectedRoute from '@components/common/ProtectedRoute';
 import Home from '@components/Home/Home';
+import StudyRoomList from '@components/StudyRoomList/index';
 import StudyRoom from '@components/StudyRoom/StudyRoom';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/studyroomlist',
+    element: <StudyRoomList />,
   },
   {
     path: '/studyroom',
@@ -20,7 +25,7 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <div className="font-pretendard text-gomz-black">
+    <div className="font-pretendard text-gomz-black selection:text-gomz-blue selection:bg-none">
       <RouterProvider router={router} />
     </div>
   );
