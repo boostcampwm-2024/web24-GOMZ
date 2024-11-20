74a87e095563844f863c7af64f792060ee3de8c8
suhyun-00 <unhyns00@gmail.com>
2024-11-21 00:34:03 +0900

    feat: StudyRoomList Header 구현

---
+++ client/src/components/StudyRoomList/Header.tsx
@@ -0,0 +1,28 @@
+import Header from '@components/common/Header';
+import StopWatch from '@components/common/StopWatch';
+
+const StudyRoomListHeader = ({ className }: { className?: string }) => {
+  return (
+    <Header
+      className={className}
+      title={
+        <div className="flex items-baseline gap-3">
+          <h1 className="text-2xl font-semibold">GOMZ</h1>
+        </div>
+      }
+      stopWatch={
+        <div className="text-xl font-normal">
+          <StopWatch elapsedSeconds={1000} />
+        </div>
+      }
+      userInfo={
+        <div className="flex gap-4 text-base font-normal">
+          <div>{localStorage.getItem('nickName')}</div>
+          <button className="underline-offset-4 hover:underline">로그인</button>
+        </div>
+      }
+    />
+  );
+};
+
+export default StudyRoomListHeader;
