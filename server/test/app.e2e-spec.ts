daab6472941d24f1468c6d71ce0e120c0566f59f
Choi-JY1107 <cjy11230@kakao.com>
2024-11-24 08:43:58 +0900

    test: e2e 테스트 코드 수정

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,41 +1,46 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as ioClient from 'socket.io-client';
 import { SignalingServerGateway } from '../src/signaling-server/signaling-server.gateway';
 import { StudyRoomsService } from '../src/study-room/study-room.service';
 import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from '../winston.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudyRoom } from '../src/study-room/entity/study-room.entity';
+import { StudyRoomParticipant } from '../src/study-room/entity/study-room-participant.entity';
+import { StudyRoomRepository } from '../src/study-room/repository/study-room.repository';
+import { StudyRoomParticipantRepository } from '../src/study-room/repository/study-room-participant.repository';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.test' });
 describe('시그널링 서버 e2e 테스트', () => {
   let app: INestApplication;
   let oldClient: ioClient.Socket;
   let newClient: ioClient.Socket;
 
   beforeAll(async () => {
-    const mockStudyRoomService = {
-      rooms: { 1: [] },
-      getRoomUsers(roomId) {
-        return this.rooms[roomId];
-      },
-      findUserRoom(socketId) {
-        return Object.keys(this.rooms).find((roomId) =>
-          this.rooms[roomId].some((user) => user.socketId === socketId),
-        );
-      },
-      addUserToRoom(roomId, socketId) {
-        this.rooms[roomId] ??= [];
-        this.rooms[roomId].push({ socketId });
-      },
-      removeUserFromRoom(roomId, socketId) {
-        this.rooms[roomId] = this.rooms[roomId].filter((user) => user.socketId !== socketId);
-      },
-    };
-
-    const moduleRef = await Test.createTestingModule({
-      imports: [WinstonModule.forRoot(winstonConfig)],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        // TypeORM 설정
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.DATABASE_TEST_HOST,
+          port: Number(process.env.DATABASE_TEST_PORT),
+          username: process.env.DATABASE_TEST_USER,
+          password: process.env.DATABASE_TEST_PASSWORD,
+          database: process.env.DATABASE_TEST_NAME,
+          entities: [StudyRoom, StudyRoomParticipant],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([StudyRoom, StudyRoomParticipant]),
+        WinstonModule.forRoot(winstonConfig),
+      ],
       providers: [
         SignalingServerGateway,
-        { provide: StudyRoomsService, useValue: mockStudyRoomService },
+        StudyRoomsService,
+        StudyRoomRepository,
+        StudyRoomParticipantRepository,
       ],
     }).compile();
 
@@ -47,59 +52,66 @@ describe('시그널링 서버 e2e 테스트', () => {
   });
 
   afterAll(async () => {
-    await app.close();
+    await app.close(); // 애플리케이션 종료
   });
 
-  it('WebRTC 시그널링 과정 테스트', () =>
-    new Promise<void>((resolve) => {
-      oldClient.connect();
+  it('WebRTC 시그널링 과정 테스트', (done) => {
+    oldClient.connect();
 
-      oldClient.on('connect', () => {
-        oldClient.emit('joinRoom', { roomId: '1' });
-      });
-      oldClient.on('offerRequest', ({ users }) => {
-        expect(users.length).toBe(0); // 참가자 0명 있어야함
-        newClient.connect();
-      });
-      oldClient.on('answerRequest', ({ newId, offer, newRandomId }) => {
-        expect(offer).toBe('TestSDPOffer'); // 신규 참가자가 보낸 Offer여야함
-        expect(newRandomId).toBe('NewRandomId'); // 신규 참가자의 랜덤 ID여야함
-        oldClient.emit('sendAnswer', {
-          newId,
-          answer: 'TestSDPAnswer',
-          oldRandomId: 'OldRandomId',
-        });
-        oldClient.emit('sendIceCandidate', { targetId: newId, iceCandidate: 'OldIceCandidate' });
-      });
-      oldClient.on('setIceCandidate', ({ iceCandidate }) => {
-        expect(iceCandidate).toBe('NewIceCandidate'); // 신규 참가자가 보낸 icecandidate여야함
-        oldClient.disconnect();
-      });
+    oldClient.on('connect', () => {
+      oldClient.emit('joinRoom', { roomId: '1' });
+    });
 
-      newClient.on('connect', () => {
-        newClient.emit('joinRoom', { roomId: '1' });
+    oldClient.on('offerRequest', ({ users }) => {
+      expect(users.length).toBe(0); // 참가자 0명 있어야함
+      newClient.connect();
+    });
+
+    oldClient.on('answerRequest', ({ newId, offer, newRandomId }) => {
+      expect(offer).toBe('TestSDPOffer'); // 신규 참가자가 보낸 Offer여야함
+      expect(newRandomId).toBe('NewRandomId'); // 신규 참가자의 랜덤 ID여야함
+      oldClient.emit('sendAnswer', {
+        newId,
+        answer: 'TestSDPAnswer',
+        oldRandomId: 'OldRandomId',
       });
-      newClient.on('offerRequest', ({ users }) => {
-        expect(users.length).toBe(1); // 참가자 1명 있어야함
-        newClient.emit('sendOffer', {
-          oldId: users[0].socketId,
-          offer: 'TestSDPOffer',
-          newRandomId: 'NewRandomId',
-        });
+      oldClient.emit('sendIceCandidate', { targetId: newId, candidate: 'OldIceCandidate' });
+    });
+
+    oldClient.on('setIceCandidate', ({ iceCandidate }) => {
+      expect(iceCandidate).toBe('NewIceCandidate'); // 신규 참가자가 보낸 icecandidate여야함
+      oldClient.disconnect();
+    });
+
+    newClient.on('connect', () => {
+      newClient.emit('joinRoom', { roomId: '1' });
+    });
+
+    newClient.on('offerRequest', ({ users }) => {
+      expect(users.length).toBe(1); // 참가자 1명 있어야함
+      newClient.emit('sendOffer', {
+        oldId: users[0].socketId,
+        offer: 'TestSDPOffer',
+        newRandomId: 'NewRandomId',
       });
-      newClient.on('completeConnection', ({ oldId, answer, oldRandomId }) => {
-        expect(answer).toBe('TestSDPAnswer'); // 기존 참가자가 보낸 Answer여야함
-        expect(oldRandomId).toBe('OldRandomId'); // 기존 참가자의 랜덤 ID여야함
-        newClient.emit('sendIceCandidate', { targetId: oldId, iceCandidate: 'NewIceCandidate' });
-      });
-      newClient.on('setIceCandidate', ({ iceCandidate }) => {
-        expect(iceCandidate).toBe('OldIceCandidate'); // 기존 참가자가 보낸 icecandidate여야함
-      });
-      newClient.on('userDisconnected', ({ targetId }) => {
-        expect(typeof targetId).toBe('string');
-        expect(targetId).not.toBe(newClient.id);
-        newClient.disconnect();
-        resolve();
-      });
-    }));
+    });
+
+    newClient.on('completeConnection', ({ oldId, answer, oldRandomId }) => {
+      expect(answer).toBe('TestSDPAnswer'); // 기존 참가자가 보낸 Answer여야함
+      expect(oldRandomId).toBe('OldRandomId'); // 기존 참가자의 랜덤 ID여야함
+      newClient.emit('sendIceCandidate', { targetId: oldId, candidate: 'NewIceCandidate' });
+    });
+
+    newClient.on('setIceCandidate', ({ iceCandidate }) => {
+      expect(iceCandidate).toBe('OldIceCandidate'); // 기존 참가자가 보낸 icecandidate여야함
+    });
+
+    newClient.on('userDisconnected', ({ targetId }) => {
+      expect(typeof targetId).toBe('string');
+      expect(targetId).not.toBe(newClient.id);
+      newClient.disconnect();
+
+      setTimeout(() => done(), 2000);
+    });
+  });
 });
