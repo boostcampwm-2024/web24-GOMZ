a0720a847ce10f35831a9208fe156b50ceb0761d
Choi-JY1107 <cjy11230@kakao.com>
2024-11-24 04:41:46 +0900

    refactor: chatting-server 내에서 service 로직 분리

--- server/src/chatting-server/chatting-server.module.ts
+++ server/src/chatting-server/chatting-server.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChattingServerGateway } from './chatting-server.gateway';
 import { StudyRoomModule } from '../study-room/study-room.module';
+import { ChattingServerService } from './chatting-server.service';
 
 @Module({
   imports: [StudyRoomModule],
-  providers: [ChattingServerGateway],
+  providers: [ChattingServerGateway, ChattingServerService],
 })
 export class ChattingServerModule {}
