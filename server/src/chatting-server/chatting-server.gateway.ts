27781bad3640296e2d6aeb01bb4845420bcbaa51
Choi-JY1107 <cjy11230@kakao.com>
2024-11-24 04:41:46 +0900

    refactor: chatting-server 내에서 service 로직 분리

--- server/src/chatting-server/chatting-server.gateway.ts
+++ server/src/chatting-server/chatting-server.gateway.ts
@@ -6,44 +6,33 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { StudyRoomsService } from '../study-room/study-room.service';
 import { Inject } from '@nestjs/common';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { SendMessageDto } from './chatting-server.dto';
+import { ChattingServerService } from './chatting-server.service';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChattingServerGateway {
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER)
     private readonly logger: Logger,
-    private readonly studyRoomsService: StudyRoomsService,
+    private readonly chattingServerService: ChattingServerService,
   ) {}
 
   @WebSocketServer()
   server: Server;
 
-  // 메시지 전송
   @SubscribeMessage('sendMessage')
   async handleSendMessage(
     @ConnectedSocket() client: Socket,
     @MessageBody() sendMessageDto: SendMessageDto,
   ) {
-    const { message } = sendMessageDto;
-    const roomId = await this.studyRoomsService.findUserRoom(client.id);
+    const userList = await this.chattingServerService.handleSendMessage(client.id);
 
-    if (!roomId) {
-      this.logger.info(`사용자 ${client.id}가 속한 방이 없습니다.`);
-      return;
-    }
-
-    const users = await this.studyRoomsService.getRoomUsers(roomId);
-    const userList = users.map((user) => user.socketId);
-
-    this.logger.info(`Message from ${client.id} in room ${userList}: ${message}`);
     client.broadcast.to(userList).emit('receiveMessage', {
       userId: client.id,
-      message,
+      message: sendMessageDto.message,
     });
   }
 }
