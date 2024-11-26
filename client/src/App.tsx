3a29b1caca0c83845b16eed3ad0979cbe7391779
Geunwoo Kim <9eunwoo.kim@gmail.com>
2024-11-26 09:36:51 +0900

    fix: 공부방 페이지에서 에러 발생 시 공부방 리스트 페이지로 이동하도록 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import ProtectedRoute from '@components/common/ProtectedRoute';
 import Home from '@components/Home/Home';
 import StudyRoomList from '@components/StudyRoomList/index';
@@ -24,6 +24,7 @@ const router = createBrowserRouter([
         <StudyRoom />
       </ProtectedRoute>
     ),
+    errorElement: <Navigate to="/studyroomlist" replace />,
   },
 ]);
 
